c7135a5b6408a354f8609fb89825cbd0e25a87cd
Taylor <550989428@qq.com>
2023-09-21 14:35:27 +0800

    new project

---
+++ src/main/java/com/niceboat/javawebcase/JavaWebCaseApplication.java
@@ -0,0 +1,13 @@
+package com.niceboat.javawebcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niceboat/javawebcase/JavaWebCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niceboat.javawebcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
